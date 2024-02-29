2ad01cf74ee81d335ac6f2fa53c6c9672095ae43
Landic <danil.volkov.235@gmail.com>
2024-02-29 12:21:33 +0200

    done

--- Volkov_HW_JS_4/task1/script.js
+++ Volkov_HW_JS_4/task1/script.js
@@ -1,17 +1,15 @@
-
-    document.addEventListener("click", function (event) {
-        if (event.target.tagName === "LI" && event.target.classList.contains("folder")) {
-            let sublist = event.target.querySelector("ul");
-            if (sublist) sublist.classList.toggle("collapsed");
-        }
+document.addEventListener("click", function (event) {
+    if (event.target.tagName === "LI" && event.target.classList.contains("folder")) {
+        let sublist = event.target.querySelector("ul");
+        if (sublist) sublist.classList.toggle("collapsed");
+    }
+});
+let listItems2 = document.querySelectorAll("li#b");
+listItems2.forEach(function (item) {
+    item.addEventListener("mouseover", function (event) {
+        event.target.classList.add("bold");
     });
-
-    let listItems2 = document.querySelectorAll("li#b");
-    listItems2.forEach(function (item) {
-        item.addEventListener("mouseover", function (event) {
-            event.target.classList.add("bold");
-        });
-        item.addEventListener("mouseout", function (event) {
-            event.target.classList.remove("bold");
-        });
+    item.addEventListener("mouseout", function (event) {
+        event.target.classList.remove("bold");
     });
+});
